209ed619f75f106d922fd8ca60280c4ea0217f19
Ira Akulova <ira.akulova2019@gmail.com>
2023-08-10 14:53:51 +0300

    added modal

--- src/components/userMenu/UserMenu.jsx
+++ src/components/userMenu/UserMenu.jsx
@@ -1,10 +1,42 @@
-// import RegisterView from "../../pages/RegisterView";
+import { useState } from "react";
+import RegisterView from "../../pages/RegisterView";
 
 const UserMenu = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+//   const onRegisterClick = () => {
+//     console.log(1);
+//   };
+//   const onLoginClick = () => {
+//     console.log(2);
+//   };
+
   return (
     <>
-      <button type="button">Register</button>
-      <button type="button">Log In</button>
+      {showModal && (
+        // <Modal toggleModal={toggleModal}>
+          <RegisterView toggleModal={toggleModal} />
+        // </Modal>
+      )}
+      <button
+        type="button"
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        Register
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        Log In
+      </button>
     </>
   );
 };
